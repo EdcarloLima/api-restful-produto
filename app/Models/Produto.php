694b33fadde20adc8b1379e870a9dac30edd34d1
Edcarlo Lima <edcarlolimsan@gmail.com>
2021-09-28 08:47:16 -0300

    Corrigindo acesso ao atributo do model

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -6,9 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    public $table      = 'produto';
-    public $primaryKey = 'id';
-    public $timestamps = false;
+    protected $table      = 'produto';
+    protected $primaryKey = 'id';
+    public $timestamps    = false;
 
     /**
      * Colunas permitidas para inserção em massa (Request)
